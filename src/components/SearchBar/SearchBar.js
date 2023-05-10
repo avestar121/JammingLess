5862e9cd5bf6bcff1e4d3fea913044e6ef1d2760
avestar <avestar121@gmail.com>
2023-05-10 13:05:10 +0200

    changes made

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,7 +15,7 @@ function SearchBar(props) {
       await props.searchSpotify(searchTerm); 
     } catch (error) {
       console.warn(error)
-      const clientId = '0744a9d113234aed9830ca9b36b3be57';
+      const clientId = '436316c449eb4e74b83ef0926ad068d4';
       const currentUrl = window.location.href;
       window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${currentUrl}`;
     }
